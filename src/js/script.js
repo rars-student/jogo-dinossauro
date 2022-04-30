bb1cd5cf35ca676cbebc267dfd6ac36415319479
Raphael Rodrigues <rars.student@gmail.com>
2022-04-30 18:21:21 -0300

    Manipulando script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
 let personagem = document.querySelector("#personagem");
 let obstaculo = document.querySelector("#obstaculo");
+
+const pular = () => {
+  if ((personagem.classList = !"animar")) {
+    personagem.classList.add("animar");
+  }
+
+  setTimeout(() => {
+    personagem.classList.remove("animar");
+  }, 500);
+};
+
+let testarColisao = setInterval(() => {
+  let topoPersonagem = parseInt(
+    window.getComputedStyle(personagem)
+  );
+});
